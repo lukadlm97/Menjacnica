58926feef115b2138aed86bcb6455c0964f46fd2
lukadlm97 <lukadlm97@gmail.com>
2018-03-09 19:21:07 +0100

    Promenjena set metoda u Klasi Valuta jer je uvidjeno to da se moze setovati i pojedinacni kurs a ne samo neka lista kurseva. Implementriane metode intefejsa Menjacnica u odgovarajucoj klasi.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -8,20 +8,48 @@ public class Menjacnica implements menjacnica.interfejsi.Menjacnica{
 
 	@Override
 	public void dodavanjeKursaValuteNaOdredjeniDan(Valuta valuta, Kurs kurs) {
-		// TODO Auto-generated method stub
+		for(int i=0;i<valute.size();i++) {
+			if(valute.get(i).getNazivValute().equals(valuta.getNazivValute()) ) {
+				valute.get(i).setKursneListe(kurs);
+			}
+		}
 		
 	}
 
 	@Override
 	public void brisanjeKursaValuteNaOdredjeniDan(GregorianCalendar datumZaBrisanje, Valuta valuta) {
-		// TODO Auto-generated method stub
-		
+		boolean obrisan=false;
+		for(int i=0;i<valute.size();i++) {
+			if(valute.get(i).getNazivValute().equals(valuta.getNazivValute())) {
+				int k=valute.get(i).getKursneListe().size();
+				for(int j=0;j<k;j++) {
+					if(valute.get(i).getKursneListe().get(j).getDatumIzdanjaListe().equals(datumZaBrisanje)) {
+						valute.remove(valute.get(i));
+						obrisan=true;
+						break;
+					}
+				}
+				if(obrisan==true)
+					break;
+			}
+		}
 	}
 
 	@Override
 	public Kurs vracanjeKursaNaOdredjeniDan(GregorianCalendar datumZaDobijanjeKursa, Valuta valuta) {
-		// TODO Auto-generated method stub
-		return null;
+		Kurs trazeniKurs=new Kurs();
+		trazeniKurs=null;
+		for(int i=0;i<valute.size();i++) {
+			if(valute.get(i).getKursneListe().equals(valuta.getNazivValute())) {
+				int k=valute.get(i).getKursneListe().size();
+				for(int j=0;j<k;j++) {
+					if(valute.get(i).getKursneListe().get(j).getDatumIzdanjaListe().equals(datumZaDobijanjeKursa)) {
+						trazeniKurs=valute.get(i).getKursneListe().get(j);
+					}
+				}
+			}
+		}
+		return trazeniKurs;
 	}
 	
 	
