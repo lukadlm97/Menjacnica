c9d96c920f5620d16c59a35b01000657e54ae032
lukadlm97 <lukadlm97@gmail.com>
2018-03-09 18:30:06 +0100

    Redefinisane odgovarajuce toString, equals i hashCode za obe klase.

--- Menjacnica/src/menjacnica/Kurs.java
+++ Menjacnica/src/menjacnica/Kurs.java
@@ -39,6 +39,56 @@ public class Kurs {
 	public void setKupovniKurs(double kupovniKurs) {
 		this.kupovniKurs = kupovniKurs;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datumIzdanjaListe == null) ? 0 : datumIzdanjaListe.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(prodajniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(srednjiKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kurs other = (Kurs) obj;
+		if (datumIzdanjaListe == null) {
+			if (other.datumIzdanjaListe != null)
+				return false;
+		} else if (!datumIzdanjaListe.equals(other.datumIzdanjaListe))
+			return false;
+		if (Double.doubleToLongBits(kupovniKurs) != Double.doubleToLongBits(other.kupovniKurs))
+			return false;
+		if (Double.doubleToLongBits(prodajniKurs) != Double.doubleToLongBits(other.prodajniKurs))
+			return false;
+		if (Double.doubleToLongBits(srednjiKurs) != Double.doubleToLongBits(other.srednjiKurs))
+			return false;
+		return true;
+	}
 	
-	
+	@Override
+	public String toString() {
+		String tekst="";
+		tekst+="\n";
+		tekst+="Kurs na dan: \n";
+		tekst+="Datum: "+datumIzdanjaListe;
+		tekst+="\n";
+		tekst+="Prodajni kurs: "+prodajniKurs;
+		tekst+="\n";
+		tekst+="Srednji kurs: "+srednjiKurs;
+		tekst+="\n";
+		tekst+="Kupovni kurs: "+kupovniKurs;
+		tekst+="\n";
+		return tekst;
+	}
 }
--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -30,5 +30,45 @@ public class Valuta {
 	public void setKursneListe(LinkedList<Kurs> kursneListe) {
 		this.kursneListe = kursneListe;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nazivValute == null) ? 0 : nazivValute.hashCode());
+		result = prime * result + ((skracenicaValute == null) ? 0 : skracenicaValute.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (nazivValute == null) {
+			if (other.nazivValute != null)
+				return false;
+		} else if (!nazivValute.equals(other.nazivValute))
+			return false;
+		if (skracenicaValute == null) {
+			if (other.skracenicaValute != null)
+				return false;
+		} else if (!skracenicaValute.equals(other.skracenicaValute))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		String tekst="";
+		tekst+="Naziv valute: "+nazivValute;
+		tekst+="\n";
+		tekst+="Skracenica valute: "+skracenicaValute;
+		for(int i=0;i<kursneListe.size();i++) {
+			kursneListe.get(i).toString();
+		}
+		return tekst;
+	}
 	
 }
