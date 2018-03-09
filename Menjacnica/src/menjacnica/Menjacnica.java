79003845f2d4c91bcb1e579feb72ecfe15e20f44
lukadlm97 <lukadlm97@gmail.com>
2018-03-09 18:54:14 +0100

    Prepravljeni ulazni parametri kod metoda interfejsa.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,25 +1,29 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 public class Menjacnica implements menjacnica.interfejsi.Menjacnica{
+	LinkedList<Valuta> valute=new LinkedList<Valuta>();
 
 	@Override
-	public void dodavanjeKursaValuteNaOdredjeniDan(Kurs kursZaUbacivanje) {
+	public void dodavanjeKursaValuteNaOdredjeniDan(Valuta valuta, Kurs kurs) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void brisanjeKursaValuteNaOdredjeniDan(GregorianCalendar datumZaBrisanje) {
+	public void brisanjeKursaValuteNaOdredjeniDan(GregorianCalendar datumZaBrisanje, Valuta valuta) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public Kurs vracanjeKursaNaOdredjeniDan(GregorianCalendar datumZaDobijanjeKursa) {
+	public Kurs vracanjeKursaNaOdredjeniDan(GregorianCalendar datumZaDobijanjeKursa, Valuta valuta) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	
 
 }
--- Menjacnica/src/menjacnica/interfejsi/Menjacnica.java
+++ Menjacnica/src/menjacnica/interfejsi/Menjacnica.java
@@ -3,9 +3,10 @@ package menjacnica.interfejsi;
 import java.util.GregorianCalendar;
 
 import menjacnica.Kurs;
+import menjacnica.Valuta;
 
 public interface Menjacnica {
-	public void dodavanjeKursaValuteNaOdredjeniDan(Kurs kursZaUbacivanje);
-	public void brisanjeKursaValuteNaOdredjeniDan(GregorianCalendar datumZaBrisanje);
-	public Kurs vracanjeKursaNaOdredjeniDan(GregorianCalendar datumZaDobijanjeKursa);
+	public void dodavanjeKursaValuteNaOdredjeniDan(Valuta valuta,Kurs kurs);
+	public void brisanjeKursaValuteNaOdredjeniDan(GregorianCalendar datumZaBrisanje,Valuta valuta);
+	public Kurs vracanjeKursaNaOdredjeniDan(GregorianCalendar datumZaDobijanjeKursa,Valuta valuta);
 }
